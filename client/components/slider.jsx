1edb5a9e594ea44b82a45d5289e81d8382e9ec2d
pramacha <archanakannan1986@gmail.com>
2018-07-19 19:05:36 -0700

    format dash board

--- client/components/slider.jsx
+++ client/components/slider.jsx
@@ -1,17 +1,33 @@
 
 
-
 import React from 'react';
+import { openModal } from '../actions/modal_actions';
 
 const Slider = () => {
+  let arr = ["Surprise your kids", "Get them new toys everyday", "New toys on a budget"];
 
+  const handleClick = (e) => {
+    console.log("clicked");
+    console.log(openModal);
+    return(
+      openModal({modal: 'signup'})
+    );
+  };
 
   return (
     <div className="slider-wrapper">
       <section className="slider-main">
 
         <div className="slider-container">
-
+          <div className="textbox">
+            <ul className="listclass">
+              <h4>Why toy-for-toy?</h4>
+              <li>Get a new toy everyday</li>
+              <li>Toys for every budget</li>
+              <li>Rent your unused toys and make money</li>
+            </ul>
+          <p className="joinbutton" onClick={(e) => handleClick(e)}>Join now</p>
+          </div>
         </div>
       </section>
     </div>
