3f9303d45ba60e0ab9b44a4b3ccbfc1fc90f6a12
pramacha <archanakannan1986@gmail.com>
2018-07-19 23:57:35 -0700

    add local host 5000 in backend and frontend

--- config/main.js
+++ config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'super secret passphrase',
   mongouri: 'mongodb://toyfortoy:toyfortoy1@ds239931.mlab.com:39931/toy-for-toy',
-  "port": process.env.PORT || 3000
+  "port": process.env.PORT || 5000
 };
