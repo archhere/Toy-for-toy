62e33dc0bfefa76ac0b28815709b57bf83ea707b
pramacha <archanakannan1986@gmail.com>
2018-07-20 06:39:12 -0700

    changed local host settings again

--- client/util/toy_util.js
+++ client/util/toy_util.js
@@ -4,7 +4,7 @@ let token = cookie.load('token');
 export const createToy = (toy) => {
   return $.ajax({
     type: 'POST',
-    url: 'http://localhost:5000/api/toys',
+    url: 'api/toys',
     dataType: 'html',
     contentType: "application/json; charset=utf-8",
     data: JSON.stringify(toy),
@@ -15,7 +15,7 @@ export const createToy = (toy) => {
 export const fetchAllToys = () => {
   return $.ajax({
     method: 'GET',
-    url: 'http://localhost:5000/api/toys',
+    url: 'api/toys',
     dataType: 'html',
     contentType: "application/json; charset=utf-8",
     headers: { "Authorization": token },
@@ -27,7 +27,7 @@ export const fetchOneToy = (id) => {
   return $.ajax({
     method: 'GET',
     // data: JSON.stringify(plan),
-    url: `http://localhost:5000/api/toys/${id}`,
+    url: `api/toys/${id}`,
     dataType: 'html',
     contentType: "application/json; charset=utf-8",
     headers: { "Authorization": token },
@@ -37,7 +37,7 @@ export const fetchOneToy = (id) => {
 export const updateToy = (toy) => {
   return $.ajax({
     method: 'PATCH',
-    url: `http://localhost:5000/api/toys/${toy._id}`,
+    url: `api/toys/${toy._id}`,
     dataType: 'html',
     contentType: "application/json; charset=utf-8",
     data: JSON.stringify(toy),
@@ -51,7 +51,7 @@ export const updateToy = (toy) => {
 export const deleteToy = id => {
   return $.ajax({
     method: 'DELETE',
-    url: `http://localhost:5000/api/toys/${id}`,
+    url: `api/toys/${id}`,
     dataType: 'html',
     contentType: "application/json; charset=utf-8",
     headers: {
