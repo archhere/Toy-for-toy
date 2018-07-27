4f80d75138cbc5d5f435d29a6124c3f9679996d6
pramacha <archanakannan1986@gmail.com>
2018-07-26 23:16:14 -0700

    add reservation

--- client/components/listings/listings_index.jsx
+++ client/components/listings/listings_index.jsx
@@ -39,6 +39,22 @@ class ListingsIndex extends React.Component {
   renderListingItem() {
 
     let currentListings = this.props.listings;
+    let listingIds = [];
+    currentListings.forEach(listing => listingIds.push(listing._id));
+    let currentLeases = this.props.lease.filter(lease => (listingIds.includes(lease.toy_id) && Date.parse(lease.end_date) >= Date.parse(new Date()) ));
+    let IdOfToysWithLeases = [];
+    currentLeases.forEach(lease => IdOfToysWithLeases.push(lease.toy_id));
+    console.log(IdOfToysWithLeases);
+
+    // console.log(currentLeases);
+    // if (currentLeases[0]) {
+    //   console.log(Date.parse(currentLeases[0].start_date));
+    //   console.log(Date.parse(new Date()));
+    //   console.log(Date.parse(new Date()) - Date.parse(currentLeases[0].start_date));
+    //   // 100000000
+    // }
+
+
 
 
     return currentListings.map( listing =>
@@ -46,10 +62,20 @@ class ListingsIndex extends React.Component {
       <div className="category-grid">
         <div className="category-holder1">
           <img className="home-category1" src={listing.img_url}></img>
+
+          {IdOfToysWithLeases.includes(listing._id) ?
+
+          <div className="home-text-container2">
+            <p className="home-category-title" className="home-category-title">{listing.description}</p>
+            <p className="options-near-me1">Available Later</p>
+          </div>
+              :
           <div className="home-text-container1">
             <p className="home-category-title" className="home-category-title">{listing.description}</p>
             <p className="options-near-me" onClick={() => this.props.openModal({modal: 'createLease',toy: listing})}>Click to reserve</p>
           </div>
+              }
+
         </div>
       </div>
     </div>
